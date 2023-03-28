37eb680f20a2cb526916c0e62e175b0476755190
Siarhei Karol <siarhei_karol@epam.com>
2023-03-28 17:41:04 +0500

    fix API path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://1scfkz9lpi.execute-api.us-east-1.amazonaws.com/dev",
+  import: "https://b90p19ybvh.execute-api.us-east-1.amazonaws.com/dev",
   bff: "https://9j2i7bry59.execute-api.us-east-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
